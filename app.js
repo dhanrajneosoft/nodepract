33ff0f6718648d326ac155d2a0957d170c75f372
Dhanraj Dantela <dhanraj.dantela@neosofttech.com>
2019-10-21 15:59:56 +0530

    deploying

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const router = express.Router();
 const routes = require("./routes/index");
-const environment = process.env.NODE_ENV; // development
+const environment = "development"; // development
 const stage = require("./config")[environment];
 app.use(bodyParser.json());
 app.use(
