5548b0ce55d8179608547495a6e646d29089a525
Dhanraj Dantela <dhanraj.dantela@neosofttech.com>
2019-10-31 19:22:11 +0530

    port changed

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ if (environment !== "production") {
   app.use(logger("dev"));
 }
 app.use("/api/v1", routes(router));
-app.listen(`${stage.port}`, () => {
+app.listen( process.env.PORT || stage.port, () => {
   // console.log(`Server now listening at localhost: ${stage.port}`);
 });
 module.exports = app;
