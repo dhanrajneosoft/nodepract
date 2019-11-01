09985b12be77e09ce026e32e8e3cc3b359b15933
Dhanraj Dantela <dhanraj.dantela@neosofttech.com>
2019-11-01 14:59:50 +0530

    production environment creating for hereko

--- config.js
+++ config.js
@@ -2,5 +2,9 @@ module.exports = {
   development: {
     port: process.env.NODE_PORT || 8081,
     saltingRounds: 10
+  },
+  production: {
+    port: process.env.NODE_PORT || 8081,
+    saltingRounds: 10
   }
 };
